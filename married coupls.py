81093e248a81b3e9dbb94c8661577327c397cc89
IAM78 <91953698+IAM78@users.noreply.github.com>
2021-11-17 14:15:35 +0330

    Add files via upload

---
+++ married coupls.py
@@ -0,0 +1,36 @@
+import random
+from colorama import Fore
+
+
+
+
+
+boys = ['ali', 'reza', 'yasin', 'benyamin', 'mehrdad', 'sajjad', 'aidin', 'shahin']
+girls = ['sara', 'zari', 'neda', 'homa', 'eli', 'goli', 'mary', 'mina']
+result=[]
+
+
+
+
+
+
+for i in range(len(boys)):
+
+    b = random.choice(boys)
+    g = random.choice(girls)
+    boys.remove(b)
+    girls.remove(g)
+    m=[b,g]
+    result.append(m)
+
+
+
+
+
+
+for j in range(len(result)):
+
+    print(Fore.LIGHTGREEN_EX+'new couple:\n\n')
+    print(Fore.BLACK+'Groom: '+Fore.BLUE+result[j][0])
+    print(Fore.RED+'          &')
+    print(Fore.WHITE+'Bride: '+Fore.BLUE+result[j][1]+'\n\n')
